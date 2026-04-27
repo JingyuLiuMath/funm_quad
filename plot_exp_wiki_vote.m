63f4d9c3e309c3924be3d7820fb14f6a88ccb21f
JingyuLiu <381258337@qq.com>
2026-04-27 13:13:11 +0100

    update wiki_vote example

---
+++ plot_exp_wiki_vote.m
@@ -0,0 +1,136 @@
+clear;
+close all;
+
+truncation_length_list = [2, 1, 0];
+method_list = [...
+    "fix_FOM", ...
+    "fix_sFOM_s", ...
+    "ada_sFOM_t"
+    ];
+
+data_prefix = "./data/wiki_vote_exp/";
+figure_prefix = "./figure/wiki_vote_exp/wiki_vote_exp";
+
+results_list = [];
+
+fprintf("truncation_length: ");
+for it = 1 : length(truncation_length_list)
+    fprintf("%d ", truncation_length_list(it));
+end
+fprintf("\n");
+fprintf("file_prefix: %s\n", data_prefix);
+
+%% Load matrix.
+load('./data/wiki_vote/wiki-Vote.mat');
+load('./data/wiki_vote/wiki-Vote-comp.mat');
+
+A = -Problem.A; ee = -ee; N = size(A,1); 
+b = ones(N,1);
+f_ex = ex_expm;
+
+%% print methods.
+num_truncate_len = length(truncation_length_list);
+num_method = length(method_list);
+for it_method = 1 : num_method
+    method_name = method_list(it_method);
+    if endsWith(method_name, "_t")
+        for it_trunc = 1 : num_truncate_len
+            truncation_length = truncation_length_list(it_trunc);
+            save_name = method_name + "_" + string(truncation_length);
+            load(data_prefix + save_name  + ".mat");
+            results_list = [results_list; result];
+        end
+    else
+        truncation_length = inf;
+        save_name = method_name;
+        load(data_prefix + save_name  + ".mat");
+        results_list = [results_list; result];
+    end
+end
+
+plot_result(results_list, f_ex, figure_prefix);
+
+function plot_result(results_list, f_ex, figure_prefix)
+
+num_result = length(results_list);
+
+color_list = get_colors(num_result);
+marker_list = get_markers(num_result);
+
+figure();
+for it = 1 : num_result
+    curr_result = results_list(it);
+    display_name = replace(curr_result.save_name, "_", "-");
+
+    loglog(vecnorm(curr_result.out.appr - f_ex) / norm(f_ex), ...
+        '--', ...
+        "Color", color_list(it, :), ...
+        "Marker", marker_list(it, :), ...
+        "DisplayName", display_name);
+    hold on;
+end
+legend("location", "southeastoutside");
+xlabel('cycle');
+ylabel('rel error');
+file_name = figure_prefix + "_rel_err.eps";
+saveas(gcf, file_name, "epsc");
+
+figure();
+for it = 1 : num_result
+    curr_result = results_list(it);
+    display_name = replace(curr_result.save_name, "_", "-");
+
+    loglog(curr_result.out.update, ...
+        '--', ...
+        "Color", color_list(it, :), ...
+        "Marker", marker_list(it, :), ...
+        "DisplayName", display_name);
+    hold on;
+end
+legend("location", "southeastoutside");
+xlabel('cycle');
+ylabel('update norm');
+file_name = figure_prefix + "_norm_update.eps";
+saveas(gcf, file_name, "epsc");
+
+figure();
+for it = 1 : num_result
+    curr_result = results_list(it);
+    display_name = replace(curr_result.save_name, "_", "-");
+
+    plot(curr_result.out.num_quadpoints, ...
+        '--', ...
+        "Color", color_list(it, :), ...
+        "Marker", marker_list(it, :), ...
+        "DisplayName", display_name);
+    hold on;
+end
+legend("location", "southeastoutside");
+xlabel('cycle');
+ylabel('num of quad points');
+file_name = figure_prefix + "_num_quad.eps";
+saveas(gcf, file_name, "epsc");
+
+figure();
+for it = 1 : num_result
+    curr_result = results_list(it);
+    display_name = replace(curr_result.save_name, "_", "-");
+    
+    prefix_char = char(curr_result.method);
+    ada_flag = strcmp(prefix_char(1:3), 'ada');
+    if ada_flag
+        plot(curr_result.out.dim, ...
+            '--', ...
+            "Color", color_list(it, :), ...
+            "Marker", marker_list(it, :), ...
+            "DisplayName", display_name);
+        hold on;
+    end
+end
+legend("location", "southeastoutside");
+xlabel('cycle');
+ylabel('subspace dim');
+file_name = figure_prefix + "_subspace_dim.eps";
+saveas(gcf, file_name, "epsc");
+
+end
---
+++ print_exp_wiki_vote.m
@@ -0,0 +1,76 @@
+clear;
+close all;
+
+truncation_length_list = [2, 1, 0];
+method_list = [...
+    "fix_FOM", ...
+    "fix_sFOM_s", ...
+    "ada_sFOM_t"
+    ];
+
+data_prefix = "./data/wiki_vote_exp/";
+
+fprintf("truncation_length: ");
+for it = 1 : length(truncation_length_list)
+    fprintf("%d ", truncation_length_list(it));
+end
+fprintf("\n");
+fprintf("file_prefix: %s\n", data_prefix);
+
+%% Load matrix.
+load('./data/wiki_vote/wiki-Vote.mat');
+load('./data/wiki_vote/wiki-Vote-comp.mat');
+
+A = -Problem.A; ee = -ee; N = size(A,1); 
+b = ones(N,1);
+f_ex = ex_expm;
+
+%% print methods.
+caption_name = "Relative error, time, iteration number and oracle number" + ...
+    " of the exp function" + ...
+    " for the wiki-vote example";
+label_name = "exp_wiki_vote";
+fprintf("\n");
+fprintf("\\begin{table}[tbhp]\n")
+fprintf("\\centering\n")
+fprintf("\\begin{tabular}{ccccc}\n")
+fprintf("\\toprule\n")
+fprintf("Method & error & time & iter number & oracle number \\\\ \n")
+num_truncate_len = length(truncation_length_list);
+num_method = length(method_list);
+for it_method = 1 : num_method
+    method_name = method_list(it_method);
+    if endsWith(method_name, "_t")
+        for it_trunc = 1 : num_truncate_len
+            truncation_length = truncation_length_list(it_trunc);
+            save_name = method_name + "_" + string(truncation_length);
+            load(data_prefix + save_name  + ".mat");
+            print_result(save_name, result, f_ex);
+        end
+    else
+        truncation_length = inf;
+        save_name = method_name;
+        load(data_prefix + save_name  + ".mat");
+        print_result(save_name, result, f_ex);
+    end
+end
+fprintf("\\bottomrule\n");
+fprintf("\\end{tabular}\n");
+fprintf("\\caption{%s}\n", caption_name);
+fprintf("\\label{tab:%s}\n", label_name);
+fprintf("\\end{table}\n");
+fprintf("\n");
+
+function print_result(save_name, result, f_ex)
+
+rel_err = norm(f_ex - result.f) / norm(f_ex);
+print_name = replace(save_name, "_", "-");
+fprintf("\\midrule\n")
+fprintf("%s ", print_name );
+fprintf("& %.1e ", rel_err);
+fprintf("& %.1e ", result.time);
+fprintf("& %d ", result.num_it);
+fprintf("& %d ", result.num_oracle);
+fprintf("\\\\ \n");
+
+end
